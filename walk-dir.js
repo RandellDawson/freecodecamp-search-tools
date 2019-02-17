e5dc89c806fcb063cdf752d46952d70073db8c99
Randell Dawson <rdawson@onepathtech.com>
2019-02-16 23:56:57 -0800

    refactor: moved walkDir to own file

---
+++ walk-dir.js
@@ -0,0 +1,8 @@
+module.exports = function walkDir(dir, callback) {
+  fs.readdirSync(dir).forEach(f => {
+    let dirPath = path.join(dir, f);
+    let isDirectory = fs.statSync(dirPath).isDirectory();
+    isDirectory ?
+      walkDir(dirPath, callback) : callback(path.join(dir, f));
+  });
+};
