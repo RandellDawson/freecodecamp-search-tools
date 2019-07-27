c1acb12f73de8526d1e5db16bb0663d1d9f601d4
Randell Dawson <rdawson@onepathtech.com>
2019-07-27 16:52:44 -0700

    feat: find assert.equals missing 2nd argument

---
+++ find-assertequals-missing-2nd-arg.js
@@ -0,0 +1,50 @@
+const walkDir = require('./utils/walk-dir');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+
+let numChallenges = 0;
+let count = 0;
+let results = '';
+
+const directories = [
+  '01-responsive-web-design',
+  '02-javascript-algorithms-and-data-structures',
+  '03-front-end-libraries',
+  '04-data-visualization',
+  '08-coding-interview-prep'
+];
+
+directories.forEach(dir => {
+  walkDir('D:/Coding/fcc/curriculum/challenges/english/' + dir + '/', function (filePath) {
+    numChallenges++;
+    const code = fs.readFileSync(filePath, 'utf8');
+    const $ = cheerio.load(code);
+    let solution;
+    try {
+      solution = $('#solution').html().trim();
+    }
+    catch (error) {
+      console.log('problem with ' + filePath);
+      return;
+    }
+    if (!/^```(js|html|css)\s*\/\/\s*solution required\s*```$/.test(solution) && !filePath.includes('-projects')) {
+      const tests = code.match(/```yml\r?\ntests:(?<testSection>[\s\S]*?)```/);
+      if (tests) {
+        const testsCode = tests.groups.testSection;
+        if (/assert\.(not|strict|notstrict|deep|notdeep|equal)/i.test(testsCode)) {
+          results += filePath
+            .replace('D:\\Coding\\fcc\\', '')
+            .replace(/\\/g, '/') + '\n';
+          results += testsCode + '\n\n';
+          count++;
+        }
+      }
+
+
+    }
+  });
+});
+fs.writeFileSync('./data/assertequals.txt', results, 'utf8');
+console.log('count = ' + count);
+console.log('numChallenges = ' + numChallenges);
