ad512060f478d587c241ebe6b43ed9e7ef61961a
Randell Dawson <rdawson@onepathtech.com>
2019-07-29 21:26:59 -0700

    fix: remove need for cheerio

--- projects/move-to-forum-prep/match-up-forum-articles.js
+++ projects/move-to-forum-prep/match-up-forum-articles.js
@@ -1,5 +1,3 @@
-const cheerio = require('cheerio');
-const path = require('path');
 const fs = require('fs');
 
 const findMatchingGuideArticle = (forumArticleName) => {
@@ -9,13 +7,10 @@ const findMatchingGuideArticle = (forumArticleName) => {
   return matchData.length > 0 ? matchData[0] : null;
 };
 
-const code = fs.readFileSync('../../data/forum-articles.html', 'utf8');
+const articles = JSON.parse(fs.readFileSync('../../data/forum-article-urls.json', 'utf8'));
 
 let challengeFiles = fs.readFileSync('../../data/challenge-files.json', 'utf8');
 challengeFiles = JSON.parse(challengeFiles).articles;
-
-const $ = cheerio.load(code);
-const $articles = $('.fps-result > .fps-topic > .topic > .search-link');
 
 const results = {
   numMatches: 0,
@@ -29,29 +24,27 @@ let numMatches = 0;
 let numNonMatches = 0;
 
 let me = 0;
-$articles.each((i, elem) => {
-  const forumArticleUrl = $(elem).attr('href');
+articles.forEach(forumArticleUrl => {
   const match = forumArticleUrl.match(/challenge-guide-(?<forumArticleName>[\S]+)\/(?<forumTopicId>\d+)/);
   if (match) {
     const { forumArticleName, forumTopicId } = match.groups;
     const matchingChallengeFile = findMatchingGuideArticle(forumArticleName);
-    const fullForumUrl = 'https://www.freecodecamp.org' + forumArticleUrl;
     if (matchingChallengeFile) {
       numMatches++;
-      results.matches.push(Object.assign({ ...matchingChallengeFile }, { fullForumUrl, forumTopicId }));
+      results.matches.push(Object.assign({ ...matchingChallengeFile }, { forumArticleUrl, forumTopicId }));
     } else {
       numNonMatches++;
-      results.nonMatches.push({ forumArticleName, fullForumUrl, forumTopicId});
+      results.nonMatches.push({ forumArticleName, forumArticleUrl, forumTopicId});
     }
   } else {
-    console.log('no match: ' + 'https://www.freecodecamp.com' + forumArticleUrl);
+    //console.log('no match: ' + 'https://www.freecodecamp.com' + forumArticleUrl);
   }
 });
 results.numMatches = numMatches;
 results.numNonMatches = numNonMatches;
 results.total = numMatches + numNonMatches;
-fs.writeFileSync('../../data/forum-topics-and-challenge-files-matrix.json', JSON.stringify(results), 'utf8');
+fs.writeFileSync('../../data/forum-topics-and-challenge-files-matrix.json', JSON.stringify(results, null, '  '), 'utf8');
 console.log('matches: ' + results.matches.length);
 console.log('non-matches: ' + results.nonMatches.length);
 console.log('total: ' + (results.matches.length + results.nonMatches.length));
-console.log('me='+me);
+console.log('me = '+me);
