4680d4ab5c415c0315c5447f8debbfbcc9967580
Randell Dawson <rdawson@onepathtech.com>
2020-02-25 16:41:44 -0800

    fix: added many more comments to lookup object

--- projects/find-comments/comments-lookup.js
+++ projects/find-comments/comments-lookup.js
@@ -1,20 +1,239 @@
-module.exports = ({
-  '// Only change code above this line': true,
-  '// Only change code below this line': true,
-  '// Change this line': true,
-  '// Example': true,
-  '// Setup': true,
-  '// Add your code above this line': true,
-  '// Add your code below this line': true,
- // '/* Add your code above this line */': true,
- // '/* Add your code below this line */': true,
-  '<!--\n<h1>Hello World</h1>\n\n<h2>CatPhotoApp</h2>\n\n<p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>\n-->': true,
-  '<!-- Only change code above this line -->': true,
-  '<!-- Only change code below this line -->': true,
-  '<!-- Stacked bar chart of weekly training-->': true,
-  '<!-- Stacked bar chart will go here -->': true,
-  '/* Only change code below this line */': true,
-  '/* Only change code above this line */': true,
-  '/* Change code below this line */': true,
-  '/* Change code above this line */': true,
-});
+const COMMENT_TRANSLATIONS = {
+  '// Only change code above this line': {
+    chinese: true
+  },
+  '// Only change code below this line': {
+    chinese: true
+  },
+  '// Change code above this line': {
+    chinese: true
+  },
+  '// Change code below this line': {
+    chinese: true
+  },
+  '// Change this line': {
+    chinese: true
+  },
+  '// Setup': {
+    chinese: true
+  },
+  '/*\nGiven a function (representing the tea type) and number of cups needed, the\nfollowing function returns an array of strings (each representing the a cup of\na specific type of tea).\n*/': {
+    chinese: true
+  },
+  '// Function that returns a string representing a cup of green tea': {
+    chinese: true
+  },
+  '// Function that returns a string representing a cup of black tea': {
+    chinese: true
+  },
+  '// Entertainment sites': {
+    chinese: true
+  },
+  '// 24.44 in Celsius': {
+    chinese: true
+  },
+  '// 26 in Celsius': {
+    chinese: true
+  },
+  '// Should display \'carrot\'': {
+    chinese: true
+  },
+  '// yields true': {
+    chinese: true
+  },
+  '// Fix the code below so that it evaluates to true': {
+    chinese: true
+  },
+  '// Add a record here': {
+    chinese: true
+  },
+  '// Adds the m-th row into newArray': {
+    chinese: true
+  },
+  '// After passing the challenge experiment with myString and see how the grouping works': {
+    chinese: true
+  },
+  '// Assign 5 to oopsGlobal Here': {
+    chinese: true
+  },
+  '// Assignments': {
+    chinese: true
+  },
+  '// Get the tabs after the tab': {
+    chinese: true
+  },
+  '// Get the tabs before the tab': {
+    chinese: true
+  },
+  '// Join them together': {
+    chinese: true
+  },
+  '// Check the two consoles to see the difference. The freeCodeCamp console should have printed the variable twice, once for each test of this challenge. The browser console should only print the variable once because you cleared it first.': {
+    chinese: true
+  },
+  '// The global variable': {
+    chinese: true
+  },
+  '// Should print "nom nom nom"': {
+    chinese: true
+  },
+  '// Should print "Woof!"': {
+    chinese: true
+  },
+  '// Close third tab in video window, and join': {
+    chinese: true
+  },
+  '// Now perform the tab opening, closing, and other operations': {
+    chinese: true
+  },
+  '// Complete the method below and implement the others similarly': {
+    chinese: true
+  },
+  '// myVar is not defined outside of myLocalScope': {
+    chinese: true
+  },
+  '// Should be 64': {
+    chinese: true
+  },
+  '// Should be 77': {
+    chinese: true
+  },
+  '// Creates a 2-D array with m rows and n columns of zeroes': {
+    chinese: true
+  },
+  '// Variable declarations': {
+    chinese: true
+  },
+  '// Variable assignments': {
+    chinese: true
+  },
+  '// Declare the myGlobal variable below this line': {
+    chinese: true
+  },
+  '// Display code': {
+    chinese: true
+  },
+  '// Let\'s create three browser windows': {
+    chinese: true
+  },
+  '// Now remove the console log line to pass the test': {
+    chinese: true
+  },
+  '// Let\'s open a new tab for now': {
+    chinese: true
+  },
+
+  '<!--\n<h1>Hello World</h1>\n\n<h2>CatPhotoApp</h2>\n\n<p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>\n-->': {
+    chinese: true
+  },
+  '<!-- Only change code above this line -->': {
+    chinese: true
+  },
+  '<!-- Only change code below this line -->': {
+    chinese: true
+  },
+  '<!-- Stacked bar chart of weekly training -->': {
+    chinese: true
+  },
+  '<!-- Stacked bar chart will go here -->': {
+    chinese: true
+  },
+  '/* Only change code below this line */': {
+    chinese: true
+  },
+  '/* Only change code above this line */': {
+    chinese: true
+  },
+  '/* Change code below this line */': {
+    chinese: true
+  },
+  '/* Change code above this line */': {
+    chinese: true
+  },
+  '/* Add your code above this line */': {
+    chinese: true
+  },
+  '/* Add your code below this line */': {
+    chinese: true
+  },
+  '// Should be 80': {
+    chinese: true
+  },
+  '// Returns a proper object': {
+    chinese: true
+  },
+  '// Using s = [2, 5, 7] would be invalid': {
+    chinese: true
+  },
+  '// Setting in Fahrenheit scale': {
+    chinese: true
+  },
+  '// Should be an object': {
+    chinese: true
+  },
+  '// Should be 28.015': {
+    chinese: true
+  },
+  '// Should be [3,4,5,6,7,8,9,10]': {
+    chinese: true
+  },
+  '// Should be [1,2,3,4,5,6,7,8,9,10];': {
+    chinese: true
+  },
+  '// Open a new tab for cat memes': {
+    chinese: true
+  },
+  '// Open your browser console.': {
+    chinese: true
+  },
+  '// Pushes n zeroes into the current row to create the columns': {
+    chinese: true
+  },
+  '// Pushes the current row, which now has n zeroes in it, to the array': {
+    chinese: true
+  },
+  '// Should not be defined': {
+    chinese: true
+  },
+  '// Run and check the console': {
+    chinese: true
+  },
+  '// Social sites': {
+    chinese: true
+  },
+  '// Use console.clear() on the next line to clear the browser console.': {
+    chinese: true
+  },
+  '// Use console.log() to print the output variable.': {
+    chinese: true
+  },
+  '// We keep a record of the array inside the object': {
+    chinese: true
+  },
+  '// When you close a tab': {
+    chinese: true
+  },
+  '// When you join two windows into one window': {
+    chinese: true
+  },
+  '// Your mailbox, drive, and other work sites': {
+    chinese: true
+  },
+  '// responseFromServer is set to false to represent an unsuccessful response from a server': {
+    chinese: true
+  },
+  '// When you open a new tab at the end': {
+    chinese: true
+  },
+  '// responseFromServer is set to true to represent a successful response from a server': {
+    chinese: true
+  },
+  '// responseFromServer represents a response from a server': {
+    chinese: true
+  },
+  '// tabs is an array of titles of each site open within the window': {
+    chinese: true
+  }
+};
+
+exports.COMMENT_TRANSLATIONS = COMMENT_TRANSLATIONS;
