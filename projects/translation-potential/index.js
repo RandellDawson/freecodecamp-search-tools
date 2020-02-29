817226fd05533e0de18cdf95df9d82509c8b03b8
Randell Dawson <rdawson@onepathtech.com>
2020-02-28 18:58:31 -0800

    fix: finalized format of differences

--- projects/translation-potential/index.js
+++ projects/translation-potential/index.js
@@ -1,38 +1,21 @@
-const { parseMarkdown } = require('@freecodecamp/challenge-md-parser');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
+const matter = require('gray-matter');
+
+const {
+  getFileContentVersions,
+  getOutputFromCommand,
+  getParsedVersions,
+  findIssues
+} = require('./utils');
+
+// Only modify the following 3 variables
 const curriculumSection = 'curriculum/challenges/english/01-responsive-web-design';
-const FIND_RELEVANT_COMMITS_CMD = `git log --pretty=format:"%H" --since "DEC 1 2019" -- ${curriculumSection}`;
+const fromDate = "DEC 1 2019";
+// Specify where to store output file(s)
+const baseFilePath = 'D:/Coding/fcc-misc/search-tools/data/';
+// Only modify the above 3 variables
 
-async function getOutputFromCommand(command) {
-  // necessary to allow to work on Windows
-  delete process.platform;
-  process.platform = 'linux';
-
-  try {
-    const { stdout } = await exec(command, {
-      env: { PATH: '/C/Program Files/Git' },
-      shell: 'C:\\Program Files\\Git\\bin\\bash.exe'
-    });
-    return stdout;
-  }
-  catch (err) {
-    console.log(err);
-  };
-  process.platform = 'win32'; // return to using Windows
-}
-
-async function getFileContentVersions(commit, filepath) {
-  // Get file content for version one commit earlier than than earliest commit
-  let command = `git show ${commit}^1:${filepath}`;
-  const oldContent = await getOutputFromCommand(command);
-
-  // Get file content for current version
-  command = `git show HEAD:${filepath}`;
-  const newContent = await getOutputFromCommand(command);
-  return { oldContent, newContent };
-}
+const FIND_RELEVANT_COMMITS_CMD = `git log --pretty=format:"%H" --since "${fromDate}" -- ${curriculumSection}`;
 
 (async function () {
   const commitsStr = await getOutputFromCommand(FIND_RELEVANT_COMMITS_CMD);
@@ -40,12 +23,14 @@ async function getFileContentVersions(commit, filepath) {
   const commit = commits.slice(-1); // earliest commit
 
   /*
-  Git command to find files between one commit earlier than than earliest
+  Git command to find files between one commit earlier than the earliest
   commit and HEAD
   */
   const command = `git diff --name-only ${commit}^1 HEAD -- ${curriculumSection}`;
   const listOfFilesBetweenDiffs = await getOutputFromCommand(command);
   const filenames = listOfFilesBetweenDiffs.split('\n');
+
+  let overallContentDiffs = []; // stores differences across all files
   for (let filepath of filenames) {
     if (filepath) {
       const {
@@ -53,55 +38,25 @@ async function getFileContentVersions(commit, filepath) {
         newContent
       } = await getFileContentVersions(commit, filepath);
       
-      const baseFilePath = 'D:/Coding/fcc-misc/search-tools/data/';
-      
-      fs.writeFileSync(`${baseFilePath}old-content.md`, oldContent, 'utf8');
       const {
-        description: oldDescription,
-        instructions: oldInstructions,
-        tests: oldTests
-      } = await parseMarkdown(`${baseFilePath}old-content.md`);
+        oldParsed,
+        newParsed
+      } = await getParsedVersions(oldContent, newContent, baseFilePath);
 
-      fs.writeFileSync(`${baseFilePath}new-content.md`, newContent, 'utf8');
-      const {
-        description: newDescription,
-        instructions: newInstructions,
-        tests: newTests
-      } = await parseMarkdown(`${baseFilePath}new-content.md`);
+      const issuesFound = findIssues(oldParsed, newParsed);
 
-      let issuesFound = '';
-      if (oldDescription !== newDescription) {
-        issuesFound += 'Descriptions do not match\n';
-      }
-      if (oldInstructions !== newInstructions) {
-        issuesFound += 'Instructions do not match\n';
-      }
-      if (oldTests.length !== newTests.length) {
-        issuesFound += `Different number of tests found (old: ${oldTests.length}, new: ${newTests.length})\n`;
-      } else {
-        const nonMatchingTexts = oldTests.reduce((results, oldTest, idx) => {
-          if (oldTest.text !== newTests[idx].text) {
-            results.push({
-              testNum: idx + 1,
-              oldText: oldTest.text,
-              newText: newTests[idx].text
-            });
-          }
-          return results;
-        }, []);
-        if (nonMatchingTexts.length) {
-          issuesFound += 'The following texts do not match:\n';
-          nonMatchingTexts.forEach(({ testNum, oldText, newText }) => {
-            issuesFound += `Test #${testNum}\n`;
-            issuesFound += `  Old text: ${oldText}\n`;
-            issuesFound += `  New text: ${newText}\n`;
-          });
-        }
-      }
+      const baseGitHubUrl = 'https://github.com/freeCodeCamp/freeCodeCamp/blob/master/';
       if (issuesFound) {
-        console.log(filepath);
-        console.log(issuesFound);
+        const { data: { title: challengeTitle } } = matter(newContent);
+        const challengeLink = `[${challengeTitle}](${baseGitHubUrl + filepath})`;
+        let content = `- [ ] ${challengeLink}\n${issuesFound}`;
+        overallContentDiffs.push(content);
       }
     }
   }
-})()
+  fs.writeFileSync(
+    `${baseFilePath}potential-changes.md`,
+    overallContentDiffs.join('---\n'),
+    'utf8'
+  );
+})();
---
+++ projects/translation-potential/utils.js
@@ -0,0 +1,116 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs');
+const { parseMarkdown } = require('@freecodecamp/challenge-md-parser');
+
+async function getOutputFromCommand(command) {
+  // necessary to allow to work on Windows
+  delete process.platform;
+  process.platform = 'linux';
+
+  try {
+    const { stdout } = await exec(command, {
+      env: { PATH: '/C/Program Files/Git' },
+      shell: 'C:\\Program Files\\Git\\bin\\bash.exe'
+    });
+    return stdout;
+  }
+  catch (err) {
+    console.log(err);
+  };
+  process.platform = 'win32'; // return to using Windows
+}
+
+async function getFileContentVersions(commit, filepath) {
+  // Get file content for version one commit earlier than than earliest commit
+  let command = `git show ${commit}^1:${filepath}`;
+  const oldContent = await getOutputFromCommand(command);
+
+  // Get file content for current version
+  command = `git show HEAD:${filepath}`;
+  const newContent = await getOutputFromCommand(command);
+  return { oldContent, newContent };
+}
+
+function createTextDiffTable(oldText, newText) {
+  return `  <table>
+  <tr>
+  <th>
+  Version
+  </th>
+  <th align="left">
+  Text
+  </th>
+  </tr>
+  <tr>
+  <td align>Old</td>
+  <td align>
+
+  \`\`\`
+  ${oldText}
+  \`\`\`
+  </td>
+  </tr>
+  <td>New</td>
+  <td>
+
+  \`\`\`
+  ${newText}
+  \`\`\`
+  </td>
+  </tr>
+  </table>
+`;
+}
+
+async function getParsedVersions(oldContent, newContent, baseFilePath) {
+  fs.writeFileSync(`${baseFilePath}old-content.md`, oldContent, 'utf8');
+  const oldParsed = await parseMarkdown(`${baseFilePath}old-content.md`);
+  fs.writeFileSync(`${baseFilePath}new-content.md`, newContent, 'utf8');
+  const newParsed = await parseMarkdown(`${baseFilePath}new-content.md`);
+  return { oldParsed, newParsed };
+}
+
+function findIssues(oldParsed, newParsed) {
+  const { description: oldDescription, instructions: oldInstructions, tests: oldTests } = oldParsed;
+  const { description: newDescription, instructions: newInstructions, tests: newTests } = newParsed;
+  let issuesFound = '';
+  if (oldDescription !== newDescription) {
+    issuesFound += '- **`Description` section has changed**\n';
+  }
+  if (oldInstructions !== newInstructions) {
+    issuesFound += '- **`Instructions` section has changed**\n';
+  }
+  if (oldTests.length !== newTests.length) {
+    issuesFound += `- **Different number of tests found (old: ${oldTests.length}, new: ${newTests.length})**\n`;
+  } else {
+    const nonMatchingTexts = oldTests.reduce((results, oldTest, idx) => {
+      if (oldTest.text !== newTests[idx].text) {
+        results.push({
+          testNum: idx + 1,
+          oldText: oldTest.text,
+          newText: newTests[idx].text
+        });
+      }
+      return results;
+    }, []);
+    if (nonMatchingTexts.length) {
+      issuesFound += '- **The following test \`text\`(s) do not match:**\n';
+      nonMatchingTexts.forEach(({ testNum, oldText, newText }) => {
+        issuesFound += `  **Test ${testNum}**\n`;
+        issuesFound += createTextDiffTable(oldText, newText) + '\n';
+      });
+    }
+  }
+  return issuesFound;
+}
+
+const utils = {
+  getOutputFromCommand,
+  getFileContentVersions,
+  createTextDiffTable,
+  getParsedVersions,
+  findIssues
+}
+
+module.exports = utils;
