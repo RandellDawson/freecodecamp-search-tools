364720121584230e672b445317cd2493d97b2cd1
Randell Dawson <rdawson@onepathtech.com>
2020-09-07 18:26:58 -0700

    fix: update comments dictionary

--- projects/find-comments/comment-dictionary.js
+++ projects/find-comments/comment-dictionary.js
@@ -38,145 +38,145 @@ const COMMENT_TRANSLATIONS = {
   'Should display \'carrot\'': {
     chinese: true
   },
-  '// yields true': {
+  'yields true': {
     chinese: true
   },
-  '// Fix the code below so that it evaluates to true': {
+  'Fix the code below so that it evaluates to true': {
     chinese: true
   },
-  '// Add a record here': {
+  'Add a record here': {
     chinese: true
   },
-  '// Adds the m-th row into newArray': {
+  'Adds the m-th row into newArray': {
     chinese: true
   },
-  '// After passing the challenge experiment with myString and see how the grouping works': {
+  'After passing the challenge experiment with myString and see how the grouping works': {
     chinese: true
   },
-  '// Assign 5 to oopsGlobal Here': {
+  'Assign 5 to oopsGlobal Here': {
     chinese: true
   },
-  '// Get the tabs after the tab': {
+  'Get the tabs after the tab': {
     chinese: true
   },
-  '// Get the tabs before the tab': {
+  'Get the tabs before the tab': {
     chinese: true
   },
-  '// Join them together': {
+  'Join them together': {
     chinese: true
   },
-  '// Check the two consoles to see the difference. The freeCodeCamp console should have printed the variable twice, once for each test of this challenge. The browser console should only print the variable once because you cleared it first.': {
+  'Check the two consoles to see the difference. The freeCodeCamp console should have printed the variable twice, once for each test of this challenge. The browser console should only print the variable once because you cleared it first.': {
     chinese: true
   },
-  '// The global variable': {
+  'The global variable': {
     chinese: true
   },
-  '// Close third tab in video window, and join': {
+  'Close third tab in video window, and join': {
     chinese: true
   },
-  '// Now perform the tab opening, closing, and other operations': {
+  'Now perform the tab opening, closing, and other operations': {
     chinese: true
   },
-  '// Complete the method below and implement the others similarly': {
+  'Complete the method below and implement the others similarly': {
     chinese: true
   },
-  '// myVar is not defined outside of myLocalScope': {
+  'myVar is not defined outside of myLocalScope': {
     chinese: true
   },
-  '// Creates a 2-D array with m rows and n columns of zeroes': {
+  'Creates a 2-D array with m rows and n columns of zeroes': {
     chinese: true
   },
-  '// Variable declarations': {
+  'Variable declarations': {
     chinese: true
   },
-  '// Variable assignments': {
+  'Variable assignments': {
     chinese: true
   },
-  '// Declare the myGlobal variable below this line': {
+  'Declare the myGlobal variable below this line': {
     chinese: true
   },
-  '// Display code': {
+  'Display code': {
     chinese: true
   },
-  '// Let\'s create three browser windows': {
+  'Let\'s create three browser windows': {
     chinese: true
   },
-  '// Now remove the console log line to pass the test': {
+  'Now remove the console log line to pass the test': {
     chinese: true
   },
-  '// Let\'s open a new tab for now': {
+  'Let\'s open a new tab for now': {
     chinese: true
   },
-  '<!--\n<h1>Hello World</h1>\n\n<h2>CatPhotoApp</h2>\n\n<p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>\n-->': {
-    chinese: '<!--\n<h1>Hello World</h1>\n\n<h2>CatPhotoApp</h2>\n\n<p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>\n-->'
+  '<h1>Hello World</h1>\n\n<h2>CatPhotoApp</h2>\n\n<p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>': {
+    chinese: '<h1>Hello World</h1>\n\n<h2>CatPhotoApp</h2>\n\n<p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>'
   },
-  '<!-- Stacked bar chart of weekly training -->': {
+  'Stacked bar chart of weekly training': {
     chinese: '每周训练的条形图'
   },
-  '<!-- Stacked bar chart will go here -->': {
+  'Stacked bar chart will go here': {
     chinese: '条形图在这里'
   },
-  '/* Add your code above this line */': {
+  'Add your code above this line': {
     chinese: true
   },
-  '/* Add your code below this line */': {
+  'Add your code below this line': {
     chinese: true
   },
-  '// Using s = [2, 5, 7] would be invalid': {
+  'Using s = [2, 5, 7] would be invalid': {
     chinese: true
   },
-  '// Setting in Fahrenheit scale': {
+  'Setting in Fahrenheit scale': {
     chinese: true
   },
-  '// Open a new tab for cat memes': {
+  'Open a new tab for cat memes': {
     chinese: true
   },
-  '// Open your browser console.': {
+  'Open your browser console.': {
     chinese: true
   },
-  '// Pushes n zeroes into the current row to create the columns': {
+  'Pushes n zeroes into the current row to create the columns': {
     chinese: true
   },
-  '// Pushes the current row, which now has n zeroes in it, to the array': {
+  'Pushes the current row, which now has n zeroes in it, to the array': {
     chinese: true
   },
-  '// Run and check the console': {
+  'Run and check the console': {
     chinese: true
   },
-  '// Social sites': {
+  'Social sites': {
     chinese: true
   },
-  '// Use console.clear() on the next line to clear the browser console.': {
+  'Use console.clear() on the next line to clear the browser console.': {
     chinese: true
   },
-  '// Use console.log() to print the output variable.': {
+  'Use console.log() to print the output variable.': {
     chinese: true
   },
-  '// We keep a record of the array inside the object': {
+  'We keep a record of the array inside the object': {
     chinese: true
   },
-  '// When you close a tab': {
+  'When you close a tab': {
     chinese: true
   },
-  '// When you join two windows into one window': {
+  'When you join two windows into one window': {
     chinese: true
   },
-  '// Your mailbox, drive, and other work sites': {
+  'Your mailbox, drive, and other work sites': {
     chinese: true
   },
-  '// responseFromServer is set to false to represent an unsuccessful response from a server': {
+  'responseFromServer is set to false to represent an unsuccessful response from a server': {
     chinese: true
   },
-  '// When you open a new tab at the end': {
+  'When you open a new tab at the end': {
     chinese: true
   },
-  '// responseFromServer is set to true to represent a successful response from a server': {
+  'responseFromServer is set to true to represent a successful response from a server': {
     chinese: true
   },
-  '// responseFromServer represents a response from a server': {
+  'responseFromServer represents a response from a server': {
     chinese: true
   },
-  '// tabs is an array of titles of each site open within the window': {
+  'tabs is an array of titles of each site open within the window': {
     chinese: true
   }
 };
