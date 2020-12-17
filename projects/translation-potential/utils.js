3c4a31c874ea0d45882f662f5fc146db14d3059b
Randell Dawson <rdawson@onepathtech.com>
2020-12-17 07:03:37 -0700

    fix: added getMd function
    feat: create diff of desc and instructions

--- projects/translation-potential/utils.js
+++ projects/translation-potential/utils.js
@@ -1,33 +1,51 @@
+const stringify = require('remark-stringify');
+const rehype = require('rehype');
+const rehype2Remark = require('rehype-remark');
+
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
-const { parseMD: parseMarkdown } = require('../../freeCodeCamp/tools/challenge-md-parser/mdx');
+const { parseMD: parseNewMarkdown } = require('../../freeCodeCamp/tools/challenge-md-parser/mdx');
+const { parseMarkdown } = require('@freecodecamp/challenge-md-parser');
 
 async function getOutputFromCommand(command) {
-  console.log(command);
   try {
     const { stdout } = await exec(command);
     return stdout;
   }
   catch (err) {
     console.log('we have an error');
-    console.log(err);
+    console.log('command');
+    console.log(command + '\n');
+    return undefined
   };
 }
 
 async function getFileContentVersions(pathToFccRepo, commit, filepath) {
   // Get file content for version one commit earlier than than earliest commit
-  let command = `git -C ${pathToFccRepo} show ${commit}^1:${filepath}`;
-  const oldContent = await getOutputFromCommand(command);
+  // files before ~ mid september had .english still
+  let command1 = `git -C ${pathToFccRepo} show ${commit}^1:${filepath.replace('.md','.english.md')}`;
+  let command2 = `git -C ${pathToFccRepo} show ${commit}^1:${filepath}`;
 
+  let oldContent;
+  oldContent = await getOutputFromCommand(command1);
+
+  if (oldContent === undefined) {
+    oldContent = await getOutputFromCommand(command2);
+  }
   // Get file content for current version
   command = `git -C ${pathToFccRepo} show HEAD:${filepath}`;
   const newContent = await getOutputFromCommand(command);
-  return { oldContent, newContent };
+  command = `cat freeCodeCamp/${filepath}`;
+  let newMdxContent = await getOutputFromCommand(command);
+
+  command = `cat freeCodeCamp/${filepath.replace('/english/','/chinese/')}`
+  let newChineseMdxContent = await getOutputFromCommand(command);
+  return { oldContent, newContent, newMdxContent, newChineseMdxContent };
 }
 
 function createTextDiffTable(oldText, newText) {
-  return `  <table>
+  return `    <table>
   <tr>
   <th>
   Version
@@ -57,44 +75,69 @@ function createTextDiffTable(oldText, newText) {
 `;
 }
 
-async function getParsedVersions(oldContent, newContent, baseFilePath) {
+function showDiff(theDiff) {
+  return `\`\`\`diff\n${theDiff}\n\`\`\``;
+}
+
+async function getParsedVersions(oldContent, newContent, newMdxContent, newChineseMdxContent, baseFilePath) {
   fs.writeFileSync(`${baseFilePath}old-content.md`, oldContent, 'utf8');
   const oldParsed = await parseMarkdown(`${baseFilePath}old-content.md`);
   fs.writeFileSync(`${baseFilePath}new-content.md`, newContent, 'utf8');
   const newParsed = await parseMarkdown(`${baseFilePath}new-content.md`);
-  return { oldParsed, newParsed };
+  fs.writeFileSync(`${baseFilePath}new-mdx-content.md`, newMdxContent, 'utf8');
+  const newMdxParsed = await parseNewMarkdown(`${baseFilePath}new-mdx-content.md`);
+  fs.writeFileSync(`${baseFilePath}new-chinese-mdx-content.md`, newChineseMdxContent, 'utf8');
+  const newChineseMdxParsed = await parseNewMarkdown(`${baseFilePath}new-chinese-mdx-content.md`);  
+  return { oldParsed, newParsed, newMdxParsed, newChineseMdxParsed };
 }
 
-function findIssues(oldParsed, newParsed) {
+async function findIssues(oldParsed, newParsed, newMdxParsed, newChineseMdxParsed, fileDiff) {
   const { description: oldDescription, instructions: oldInstructions, tests: oldTests } = oldParsed;
   const { description: newDescription, instructions: newInstructions, tests: newTests } = newParsed;
+  const { tests: newMdxTests } = newMdxParsed;
+  const { tests: newChineseMdxTests } = newChineseMdxParsed;
+
   let issuesFound = '';
   if (oldDescription !== newDescription) {
-    issuesFound += '- **`Description` section has changed**\n';
+    issuesFound += `- **\`Description\` section has changed**\n`;
   }
   if (oldInstructions !== newInstructions) {
-    issuesFound += '- **`Instructions` section has changed**\n';
+    issuesFound += `- **\`Instructions\` section has changed**\n`;
   }
   if (oldTests.length === newTests.length) {
-    const nonMatchingTexts = oldTests.reduce((results, oldTest, idx) => {
-      if (oldTest.text !== newTests[idx].text) {
-        results.push({
+    const nonMatchingTexts = [];
+    for (let idx = 0; idx < oldTests.length; idx++) {
+      const mdxFormat = await getMd(newMdxTests[idx].text);
+      if (oldTests[idx].text !== newTests[idx].text) {
+        nonMatchingTexts.push({
           testNum: idx + 1,
-          oldText: oldTest.text,
-          newText: newTests[idx].text
+          oldText: oldTests[idx].text,
+          newText: mdxFormat
         });
       }
-      return results;
-    }, []);
+    }
     if (nonMatchingTexts.length) {
-      issuesFound += '- **The following test \`text\`(s) do not match:**\n';
+      issuesFound += '- **The following test(s) \`text\` do not match:**\n**Instructions:** Make sure to also check that the English tests have not been reorderd.\n';
       nonMatchingTexts.forEach(({ testNum, oldText, newText }) => {
-        issuesFound += `  **Test ${testNum}**\n`;
+        issuesFound += `**Test ${testNum}**\n`;
         issuesFound += createTextDiffTable(oldText, newText) + '\n';
       });
     }
   }
+  if (newTests.length !== newChineseMdxTests.length) {
+    const testsInfo = `English: ${newTests.length} | Chinese: ${newChineseMdxTests.length}`;
+    issuesFound += `- **Number of tests do not match - ${testsInfo}**\n**Instructions:** Add or delete the applicable tests.  It is possible some tests have been rearranged, so make sure each English test has a corresponding Chinese test in the same order\n`;
+  }
   return issuesFound;
+}
+
+const stringifyMd = rehype()
+  .use(rehype2Remark)
+  .use(stringify, { fences: true, emphasis: '*' });
+
+async function getMd(text) {
+  const result = await stringifyMd.process(text).then(file => file.contents);
+  return result;
 }
 
 const utils = {
